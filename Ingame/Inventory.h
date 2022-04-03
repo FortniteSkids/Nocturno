d00ea0f00b36fc4ff1f4cf0af007b1db435a4ced
John Shier <79332140+Array0x@users.noreply.github.com>
2022-04-03 12:16:29 -0400

    Add files via upload

---
+++ Ingame/Inventory.h
@@ -0,0 +1,226 @@
+#pragma once
+
+#include "SDK.hpp"
+#include <map>
+#include "Util.h"
+#include <iostream>
+
+//Credit: Polaris
+struct AFortAsQuickBars
+{
+public:
+    unsigned char                                      UnknownData00[0x1A88];
+    class SDK::AFortQuickBars* QuickBars;
+};
+struct WorldInventory
+{
+    unsigned char UnknownData00[0x1D28];
+    class SDK::AFortInventory* WorldInventory;
+};
+struct ParentInventory
+{
+    unsigned char UnknownData00[0x0098];
+    class SDK::AFortInventory               ParentInventory;
+};
+struct OwnerInventory
+{
+    unsigned char UnknownData00[0x01D8];
+    class SDK::AFortInventory* OwnerInventory;
+};
+struct AFortAsBuildPreview
+{
+public:
+    unsigned char UnknownData00[0x1788];
+    class SDK::ABuildingPlayerPrimitivePreview* BuildPreviewMarker;
+};
+struct AFortAsCurrentBuildable
+{
+public:
+    unsigned char UnknownData00[0x1940];
+    class SDK::UClass* CurrentBuildableClass;
+};
+struct AFortAsLastBuildable
+{
+public:
+    unsigned char UnknownData00[0x1948];
+    class SDK::UClass* PreviousBuildableClass;
+};
+struct AFortAsEditActor
+{
+public:
+    unsigned char UnknownData00[0x1A48];
+    class SDK::ABuildingSMActor* EditBuildingActor;
+};
+struct AFortAsBuildPreviewMID
+{
+public:
+    unsigned char UnknownData00[0x1928];
+    class SDK::UMaterialInstanceDynamic* BuildPreviewMarkerMID;
+};
+
+SDK::AFortQuickBars* QuickBars;
+SDK::FGuid EditToolGuid;
+SDK::FGuid PickaxeGuid;
+SDK::FGuid WallGuid;
+SDK::FGuid FloorGuid;
+SDK::FGuid StairGuid;
+SDK::FGuid RoofGuid;
+SDK::UFortWeaponMeleeItemDefinition* PickaxeDef;
+std::map<SDK::FGuid*, SDK::UFortWeaponItemDefinition*> Items;
+std::map<SDK::FGuid*, SDK::UFortTrapItemDefinition*> Traps;
+SDK::AFortInventory* FortInventory;
+
+namespace Inventory {
+    static inline void SetupInventory()
+    {
+        auto EditToolDef = SDK::UObject::FindObject<SDK::UFortEditToolItemDefinition>("FortEditToolItemDefinition EditTool.EditTool");
+        auto WallBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Wall.BuildingItemData_Wall");
+        auto FloorBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Floor.BuildingItemData_Floor");
+        auto StairBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Stair_W.BuildingItemData_Stair_W");
+        auto RoofBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_RoofS.BuildingItemData_RoofS");
+        auto pWood = SDK::UObject::FindObject<SDK::UFortWeaponItemDefinition>("FortResourceItemDefinition WoodItemData.WoodItemData");
+        auto pMetal = SDK::UObject::FindObject<SDK::UFortWeaponItemDefinition>("FortResourceItemDefinition MetalItemData.MetalItemData");
+        auto pStone = SDK::UObject::FindObject<SDK::UFortWeaponItemDefinition>("FortResourceItemDefinition StoneItemData.StoneItemData");
+        auto pRockets = SDK::UObject::FindObject<SDK::UFortWeaponMeleeItemDefinition>("FortAmmoItemDefinition AmmoDataRockets.AmmoDataRockets");
+        Items.insert_or_assign(Util::GenerateGuidPtr(), pWood);
+        Items.insert_or_assign(Util::GenerateGuidPtr(), pMetal);
+        Items.insert_or_assign(Util::GenerateGuidPtr(), pStone);
+        Items.insert_or_assign(Util::GenerateGuidPtr(), pRockets);
+
+        for (int i = 0; i < SDK::UObject::GetGlobalObjects().Num(); ++i)
+        {
+            auto object = SDK::UObject::GetGlobalObjects().GetByIndex(i);
+            if (object != nullptr && object->GetFullName().find("FortniteGame") == std::string::npos)
+            {
+                if (object->GetFullName().rfind("FortAmmoItemDefinition", 0) == 0)
+                {
+                    Items[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponItemDefinition*>(object);
+                }
+                if (object->GetFullName().rfind("FortWeaponRangedItemDefinition", 0) == 0)
+                {
+                    Items[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponItemDefinition*>(object);
+                }
+                if (object->GetFullName().rfind("FortTrapItemDefinition", 0) == 0)
+                {
+                    auto guid = Util::GenerateGuidPtr();
+                    Traps[guid] = reinterpret_cast<SDK::UFortTrapItemDefinition*>(object);
+                    Items[guid] = reinterpret_cast<SDK::UFortTrapItemDefinition*>(object);
+                }
+                if (object->GetFullName().rfind("FortWeaponMeleeItemDefinition", 0) == 0)
+                {
+                    Items[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponMeleeItemDefinition*>(object);
+                }
+            }
+        }
+
+        auto FortController = reinterpret_cast<SDK::AFortPlayerController*>(Cores::PlayerController);
+        FortInventory = reinterpret_cast<WorldInventory*>(Cores::PlayerController)->WorldInventory;
+
+        for (auto it = Items.begin(); it != Items.end(); it++)
+        {
+            SDK::UFortItem* Item;
+            SDK::UFortWorldItem* WorldItem;
+
+            if (it->second->IsA(SDK::UFortWeaponItemDefinition::StaticClass())) {
+                Item = it->second->CreateTemporaryItemInstanceBP(1, 0);
+                WorldItem = reinterpret_cast<SDK::UFortWorldItem*>(Item);
+                WorldItem->ItemEntry.Count = 1;
+            } else {
+                Item = it->second->CreateTemporaryItemInstanceBP(999, 0);
+                WorldItem = reinterpret_cast<SDK::UFortWorldItem*>(Item);
+                WorldItem->ItemEntry.Count = 999;
+            }
+
+            WorldItem->SetOwningControllerForTemporaryItem(FortController);
+
+            FortInventory->Inventory.ReplicatedEntries.Add(WorldItem->ItemEntry);
+            FortInventory->Inventory.ItemInstances.Add(WorldItem);
+
+            FortInventory->HandleInventoryLocalUpdate();
+            static_cast<SDK::AFortPlayerController*>(Cores::PlayerController)->HandleWorldInventoryLocalUpdate();
+        }
+
+        auto PickaxeItem = PickaxeDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto WorldPickaxeItem = reinterpret_cast<SDK::UFortWorldItem*>(PickaxeItem);
+        WorldPickaxeItem->ItemEntry.Count = 1;
+        FortInventory->Inventory.ReplicatedEntries.Add(WorldPickaxeItem->ItemEntry);
+        FortInventory->Inventory.ItemInstances.Add(WorldPickaxeItem);
+        QuickBars->ServerAddItemInternal(WorldPickaxeItem->GetItemGuid(), SDK::EFortQuickBars::Primary, 0);
+
+        auto WallBuildItem = WallBuildDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto WallWorldBuildItem = reinterpret_cast<SDK::UFortWorldItem*>(WallBuildItem);
+        FortInventory->Inventory.ReplicatedEntries.Add(WallWorldBuildItem->ItemEntry);
+        FortInventory->Inventory.ItemInstances.Add(WallWorldBuildItem);
+        QuickBars->ServerAddItemInternal(WallWorldBuildItem->GetItemGuid(), SDK::EFortQuickBars::Secondary, 0);
+        WallGuid = WallWorldBuildItem->GetItemGuid();
+
+        auto FloorBuildItem = FloorBuildDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto FloorWorldBuildItem = reinterpret_cast<SDK::UFortWorldItem*>(FloorBuildItem);
+        FortInventory->Inventory.ReplicatedEntries.Add(FloorWorldBuildItem->ItemEntry);
+        FortInventory->Inventory.ItemInstances.Add(FloorWorldBuildItem);
+        QuickBars->ServerAddItemInternal(FloorWorldBuildItem->GetItemGuid(), SDK::EFortQuickBars::Secondary, 1);
+        FloorGuid = FloorWorldBuildItem->GetItemGuid();
+
+        auto StairBuildItem = StairBuildDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto StairWorldBuildItem = reinterpret_cast<SDK::UFortWorldItem*>(StairBuildItem);
+        FortInventory->Inventory.ReplicatedEntries.Add(StairWorldBuildItem->ItemEntry);
+        FortInventory->Inventory.ItemInstances.Add(StairWorldBuildItem);
+        QuickBars->ServerAddItemInternal(StairWorldBuildItem->GetItemGuid(), SDK::EFortQuickBars::Secondary, 2);
+        StairGuid = StairBuildItem->GetItemGuid();
+
+        auto RoofBuildItem = RoofBuildDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto RoofWorldBuildItem = reinterpret_cast<SDK::UFortWorldItem*>(RoofBuildItem);
+        FortInventory->Inventory.ReplicatedEntries.Add(RoofWorldBuildItem->ItemEntry);
+        FortInventory->Inventory.ItemInstances.Add(RoofWorldBuildItem);
+        QuickBars->ServerAddItemInternal(RoofWorldBuildItem->GetItemGuid(), SDK::EFortQuickBars::Secondary, 3);
+        RoofGuid = RoofWorldBuildItem->GetItemGuid();
+
+        FortInventory->HandleInventoryLocalUpdate();
+        static_cast<SDK::AFortPlayerController*>(Cores::PlayerController)->HandleWorldInventoryLocalUpdate();
+    }
+
+    static inline void SetupQuickbars() 
+    {
+        auto FortController = reinterpret_cast<SDK::AFortPlayerController*>(Cores::PlayerController);
+        QuickBars = reinterpret_cast<SDK::AFortQuickBars*>(Util::SpawnActor(SDK::AFortQuickBars::StaticClass(), SDK::FVector{ 0,0,3029 }, SDK::FRotator()));
+        reinterpret_cast<AFortAsQuickBars*>(Cores::PlayerController)->QuickBars = QuickBars;
+        QuickBars->SetOwner(Cores::PlayerController);
+
+        static_cast<SDK::AFortPlayerController*>(Cores::PlayerController)->OnRep_QuickBar();
+        QuickBars->OnRep_PrimaryQuickBar();
+        QuickBars->OnRep_SecondaryQuickBar();
+    }
+
+    static inline void UpdateInventory()
+    {
+        FortInventory->HandleInventoryLocalUpdate();
+        static_cast<SDK::AFortPlayerController*>(Cores::PlayerController)->HandleWorldInventoryLocalUpdate();
+        static_cast<SDK::AFortPlayerController*>(Cores::PlayerController)->OnRep_QuickBar();
+        QuickBars->OnRep_PrimaryQuickBar();
+        QuickBars->OnRep_SecondaryQuickBar();
+    }
+
+    static inline void ExecuteInventoryItem(SDK::FGuid* Guid)
+    {
+        auto ItemInstances = FortInventory->Inventory.ItemInstances;
+
+        for (int i = 0; i < ItemInstances.Num(); i++)
+        {
+            auto ItemInstance = ItemInstances.operator[](i);
+
+            if (Util::AreGuidsTheSame(ItemInstance->GetItemGuid(), (*Guid))) 
+            {
+                Cores::PlayerPawn->EquipWeaponDefinition((SDK::UFortWeaponItemDefinition*)ItemInstance->GetItemDefinitionBP(), (*Guid));
+            }
+        }
+    }
+
+    static void DropPickupAtLocation(SDK::UFortItemDefinition* ItemDef, int Count) 
+    {
+        auto FortPickup = reinterpret_cast<SDK::AFortPickupBackpack*>(Util::SpawnActor(SDK::AFortPickupBackpack::StaticClass(), Cores::PlayerPawn->K2_GetActorLocation(), SDK::FRotator()));
+        FortPickup->PrimaryPickupItemEntry.ItemDefinition = ItemDef;
+        FortPickup->PrimaryPickupItemEntry.Count = Count;
+        FortPickup->OnRep_PrimaryPickupItemEntry();
+        FortPickup->TossPickup(Cores::PlayerPawn->K2_GetActorLocation(), Cores::PlayerPawn, 999, true);
+    }
+}
---
+++ Ingame/dllmain.cpp
@@ -0,0 +1,244 @@
+﻿#include <Windows.h>
+#include "Util.h"
+#include "minhook/MinHook.h"
+#include "Inventory.h"
+#include "HuskAI.h"
+#include <ostream>
+#include <iostream>
+
+#pragma comment(lib, "minhook/minhook.lib")
+
+PVOID(*CollectGarbageInternal)(uint32_t, bool) = nullptr;
+PVOID CollectGarbageInternalHook(uint32_t KeepFlags, bool bPerformFullPurge)
+{
+    return NULL;
+}
+
+template<typename T>
+T* FindOffSet(const std::string& sClassName, const std::string& sQuery)
+{
+    for (int i = 0; i < SDK::UObject::GetGlobalObjects().Num(); ++i)
+    {
+        auto pObject = SDK::UObject::GetGlobalObjects().GetByIndex(i);
+        if (pObject != nullptr && pObject->GetFullName().find("F_Med_Head1") == std::string::npos)
+        {
+            if (pObject->GetFullName().rfind(sClassName, 0) == 0 && pObject->GetFullName().find(sQuery) != std::string::npos)
+                return static_cast<T*>(pObject);
+        }
+    }
+
+    return nullptr;
+}
+
+bool bIsReady = false;
+bool bHasSpawned = false;
+bool bIsInGame = false;
+
+
+PVOID(*ProcessEvent)(SDK::UObject*, SDK::UFunction*, PVOID) = nullptr;
+PVOID ProcessEventHook(SDK::UObject* object, SDK::UFunction* function, PVOID params) 
+{
+    if (object && function) {
+        if (function->GetName().find("StartButton") != std::string::npos)
+        {
+            // this is the map that it loads to
+            Cores::PlayerController->SwitchLevel(L"Zone_Onboarding_FarmsteadFort");
+            bIsReady = true;
+        }
+
+        // game is set to ReadyToStartMatch
+        if (function->GetName().find("ReadyToStartMatch") != std::string::npos && bIsReady)
+        {
+            Util::InitSdk();
+            Util::InitCores();
+            //Util::InitPatches();
+
+            printf("ReadyToStartMatch!\n");
+
+            if (!bHasSpawned) {
+                // sets the game to summon the playerpawn
+                Cores::PlayerController->CheatManager->Slomo(0);
+                Cores::PlayerController->CheatManager->Summon(L"PlayerPawn_Generic_C");
+                Cores::PlayerPawn = reinterpret_cast<SDK::APlayerPawn_Generic_C*>(Util::FindActor(SDK::APlayerPawn_Generic_C::StaticClass()));
+                if (!Cores::PlayerPawn)
+                {
+                    // prints if the playerpawn cannot be summoned for some unknown reason
+                    printf("PlayerPawn not found!\n");
+                    return FALSE;
+                }
+                // allows for the game to possess the pawn and sets the player pawn's location and state
+                Cores::PlayerController->Possess(Cores::PlayerPawn);
+                Cores::PlayerController->CheatManager->BugItGo(-3600, -300, -1522, 0, 275, 0);
+                Cores::PlayerController->CheatManager->God();
+                Cores::PlayerController->CheatManager->Slomo(1);
+                printf("Pawn!\n");
+                // sets the game state for the game, which is zone
+                Cores::PlayerController->CheatManager->Summon(TEXT("FortGameStateZone"));
+                printf("State!\n");
+
+                // variables for the stw gamestate
+                auto FortPlayerController = reinterpret_cast<SDK::AFortPlayerController*>(Cores::PlayerController);
+                auto FortGameMode = reinterpret_cast<SDK::AFortGameMode*>((*Cores::World)->AuthorityGameMode);
+                auto FortPlayerState = reinterpret_cast<SDK::AFortPlayerState*>(Cores::PlayerController->PlayerState);
+                FortPlayerState->bIsGameSessionOwner = true;
+                FortPlayerState->OnRep_SessionOwner();
+                FortPlayerState->OnRep_CharacterParts();
+                Cores::PlayerPawn->OnCharacterPartsReinitialized();
+
+                // sets the pickaxe for the player pawn
+                auto pickaxeDef = SDK::UObject::FindObject<SDK::UFortWeaponMeleeItemDefinition>("FortWeaponMeleeItemDefinition WID_Harvest_Pickaxe_SR_T06.WID_Harvest_Pickaxe_SR_T06");
+                PickaxeDef = pickaxeDef;
+
+                FortGameMode->StartMatch();
+                printf("StartMatch!\n");
+                FortGameMode->StartPlay();
+                printf("StartPlay!\n");
+
+                // shows when the mission has started
+                printf("Started mission!\n");
+
+                bHasSpawned = true;
+                bIsReady = false;
+                bIsInGame = true;
+            }
+        }
+
+        if (function->GetName().find("ServerExecuteInventoryItem") != std::string::npos && bIsInGame) 
+        {
+            SDK::FGuid* guid = reinterpret_cast<SDK::FGuid*>(params);
+            Inventory::ExecuteInventoryItem(guid);
+        }
+
+        if (function->GetName().find("ServerRemoveInventoryItem") != std::string::npos && bIsInGame)
+            if (function->GetName().find("ServerAttemptInventoryDrop") != std::string::npos && bIsInGame)
+            {
+                struct Params_
+                {
+                    SDK::FGuid ItemGuid;
+                    int Count;
+                };
+                auto Params = (Params_*)(params);
+                auto ItemInstances = FortInventory->Inventory.ItemInstances;
+                auto QuickbarSlots = QuickBars->PrimaryQuickBar.Slots;
+                for (int i = 0; i < ItemInstances.Num(); i++)
+                {
+                    auto ItemInstance = ItemInstances.operator[](i);
+                    if (Util::AreGuidsTheSame(Params->ItemGuid, ItemInstance->GetItemGuid()))
+                    {
+                        Inventory::DropPickupAtLocation(ItemInstance->GetItemDefinitionBP(), Params->Count);
+                    }
+                }
+
+                for (int i = 0; i < QuickbarSlots.Num(); i++)
+                    for (int i = 0; i < QuickbarSlots.Num(); i++)
+                    {
+                        if (Util::AreGuidsTheSame(QuickbarSlots[i].Items[0], Params->ItemGuid))
+                            if (Util::AreGuidsTheSame(QuickbarSlots[i].Items[0], Params->ItemGuid))
+                            {
+                                QuickBars->EmptySlot(SDK::EFortQuickBars::Primary, i);
+                                Inventory::UpdateInventory();
+                            }
+                    }
+            }
+
+        if (function->GetName().find("ServerHandlePickup") != std::string::npos && bIsInGame)
+        {
+            struct ServerHandlePickupParams
+            {
+                SDK::UFortItemDefinition* Pickup;
+                float InFlyTime;
+                SDK::FVector InStartDirection;
+                bool bPlayPickupSound;
+            };
+
+            auto Params = (ServerHandlePickupParams*)(params);
+
+            //Inventory::AddItemToInventory(Params->Pickup, 125, 1);
+        }
+
+        if (function->GetName().find("Tick") != std::string::npos && bIsInGame) 
+        {
+            //Jumping
+            if (GetAsyncKeyState(VK_SPACE) && 0x01) {
+                if (Cores::PlayerPawn->CanJump() && !Cores::PlayerPawn->IsJumpProvidingForce()) {
+                    Cores::PlayerPawn->Jump();
+                }
+            }
+
+            // WIP HuskAI code
+            if (GetAsyncKeyState(VK_F1) && 0x01) {
+                HuskAI::SpawnHusk();
+            }
+
+            Cores::PlayerPawn->CurrentMovementStyle = reinterpret_cast<SDK::AFortPlayerController*>(Cores::PlayerController)->bWantsToSprint ? SDK::EFortMovementStyle::Walking : SDK::EFortMovementStyle::Sprinting;
+        }
+
+        if (function->GetName().find("ServerLoadingScreenDropped") != std::string::npos && bIsInGame)
+        {
+            Inventory::SetupQuickbars();
+            Inventory::SetupInventory();
+            Inventory::UpdateInventory();
+
+            auto FortController = reinterpret_cast<SDK::AFortPlayerController*>(Cores::PlayerController);
+            auto FortCheatManager = reinterpret_cast<SDK::UFortCheatManager*>(Cores::PlayerController->CheatManager);
+            FortCheatManager->CraftFree(); //Lets you craft anything but it doesn't work as of right now
+            FortCheatManager->BackpackSetSize(69420); //funny number go brr
+            FortCheatManager->GiveCheatInventory(); // gives the player all of the items in-game
+            FortCheatManager->EvolveHero(); //Evolves the hero i hope
+            FortCheatManager->GiveAllWeapons(); // gives all of the weapons
+            FortCheatManager->GiveResources(999); // gives the player maximum mats
+            FortCheatManager->GiveUsefulThings(999); // gives the player maximum items
+
+            /*auto GCADDR = Util::FindPattern("\x48\x8B\xC4\x48\x89\x58\x08\x88\x50\x10", "xxxxxxxxxx");
+            MH_CreateHook((LPVOID)(GCADDR), CollectGarbageInternalHook, (LPVOID*)(&CollectGarbageInternal));
+            MH_EnableHook((LPVOID)(GCADDR));*/
+        }
+    }
+
+    return ProcessEvent(object, function, params);
+}
+
+DWORD WINAPI MainThread(LPVOID) 
+{
+    Util::InitConsole();
+
+    auto idk = R"(    _   __           __                       
+   / | / /___  _____/ /___  ___________  ____ 
+  /  |/ / __ \/ ___/ __/ / / / ___/ __ \/ __ \
+ / /|  / /_/ / /__/ /_/ /_/ / /  / / / / /_/ /
+/_/ |_/\____/\___/\__/\__,_/_/  /_/ /_/\____/ 
+                                             )";
+    printf(idk);
+    
+    printf("\nCreated by Jacobb626 and Windermed!\n");
+    printf("Go to the Map and select any mission to load in, have fun!\n");
+
+    MH_Initialize();
+
+    Util::InitSdk();
+    Util::InitCores();
+    //Util::InitPatches();
+    Util::InitGameplaystatics();
+
+    auto ProcessEventAddress = Util::FindPattern("\x40\x55\x56\x57\x41\x54\x41\x55\x41\x56\x41\x57\x48\x81\xEC\x00\x00\x00\x00\x48\x8D\x6C\x24\x00\x48\x89\x9D\x00\x00\x00\x00\x48\x8B\x05\x00\x00\x00\x00\x48\x33\xC5\x48\x89\x85\x00\x00\x00\x00\x48\x63\x41\x0C", "xxxxxxxxxxxxxxx????xxxx?xxx????xxx????xxxxxx????xxxx");
+    if (!ProcessEventAddress) {
+        MessageBox(NULL, static_cast<LPCWSTR>(L"Finding pattern for ProcessEvent has failed, please re-open Fortnite and try again!"), static_cast<LPCWSTR>(L"Error"), MB_ICONERROR);
+        ExitProcess(EXIT_FAILURE);
+    }
+
+    MH_CreateHook((LPVOID)(ProcessEventAddress), ProcessEventHook, (LPVOID*)(&ProcessEvent));
+    MH_EnableHook((LPVOID)(ProcessEventAddress));
+
+    return TRUE;
+}
+
+// basic dll injection shit lol
+BOOL APIENTRY DllMain(HMODULE mod, DWORD reason, LPVOID res)
+{
+    if (reason == DLL_PROCESS_ATTACH) 
+    {
+        CreateThread(0, 0, MainThread, mod, 0, 0);
+    }
+
+    return TRUE;
+}
